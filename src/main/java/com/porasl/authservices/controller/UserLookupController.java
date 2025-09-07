e0983e818821d8ad23aa476be81ae7b4da6e5893
porasl <151912720+porasl@users.noreply.github.com>
2025-09-07 16:15:53 -0700

    restored the page for profileImage URl

--- src/main/java/com/porasl/authservices/controller/UserLookupController.java
+++ src/main/java/com/porasl/authservices/controller/UserLookupController.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.porasl.authservices.dto.UserDTO;
 import com.porasl.authservices.user.UserRepository;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/internal/users")
-@RequiredArgsConstructor
 class UserLookupController {
-  private final UserRepository repo;
+    private final UserRepository repo;
 
-  @GetMapping        // GET /internal/users?emails=a@x&emails=b@y
-  List<UserDTO> byEmails(@RequestParam String email) {
-    return repo.findByEmail(email).stream()
-        .map(u -> new UserDTO(u.getId(), u.getEmail(), u.getFirstname(), u.getLastname(), u.getProfileImageUrl()))
-        .toList();
-  }
+    public UserLookupController(UserRepository repo) {
+        this.repo = repo;
+    }
 
+    @GetMapping        // GET /internal/users?emails=a@x&emails=b@y
+    List<UserDTO> byEmails(@RequestParam String email) {
+        return repo.findByEmail(email).stream()
+            .map(u -> new UserDTO(u.getId(), u.getEmail(), u.getFirstname(), u.getLastname(), u.getProfileImageUrl()))
+            .toList();
+    }
 }
 
 
