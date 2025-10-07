97c5543027c7566557514ca08b48d78738c9b213
porasl <151912720+porasl@users.noreply.github.com>
2025-10-07 02:48:47 -0700

    added import ActivateRequest

--- src/main/java/com/porasl/authservices/controller/AuthenticationController.java
+++ src/main/java/com/porasl/authservices/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.porasl.authservices.auth.ActivateRequest;
 import com.porasl.authservices.auth.AuthenticationRequest;
 import com.porasl.authservices.auth.AuthenticationResponse;
 import com.porasl.authservices.auth.RegisterRequest;
