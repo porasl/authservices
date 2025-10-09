c888974a1dbe2116cadbb7e43b099c949f1bda9c
porasl <151912720+porasl@users.noreply.github.com>
2025-10-08 23:54:21 -0700

    Update of UserConnectionPropository

--- src/main/java/com/porasl/authservices/connection/UserConnectionRepository.java
+++ src/main/java/com/porasl/authservices/connection/UserConnectionRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserConnectionRepository extends JpaRepository<UserConnection, Long> {
-Optional<UserConnection> findByUserIdAndTargetUserId(Long userId, Long targetUserId);
 
 Optional<UserConnection> findByUserIdAndTargetUserIdAndStatus(
    Long userId, Long targetUserId, UserConnection.Status status);
@@ -15,4 +14,12 @@ List<UserConnection> findByUserIdAndStatus(Long userId, UserConnection.Status st
 List<UserConnection> findByTargetUserIdAndStatus(Long targetUserId, UserConnection.Status status);
 
 Optional<UserConnection> findBetweenUsers(long id, long id2);
+
+//Replacement for "findBetweenUsers"
+Optional<UserConnection> findByUserIdAndTargetUserId(Long userId, Long targetUserId);
+Optional<UserConnection> findByTargetUserIdAndUserId(Long targetUserId, Long userId);
+
+// (Optional) faster existence checks
+boolean existsByUserIdAndTargetUserId(Long userId, Long targetUserId);
+boolean existsByTargetUserIdAndUserId(Long targetUserId, Long userId);
 }
